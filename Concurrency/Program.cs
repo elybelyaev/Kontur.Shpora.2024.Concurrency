a7ce97b4473dd0a1a80f98d43e15c1d20dbf08b9
belyaev.ia <belyaev.ia@kontur.ru>
2024-02-22 19:32:51 +0500

    conprog: test for whenall

--- Concurrency/Program.cs
+++ Concurrency/Program.cs
@@ -4,3 +4,10 @@ using Concurrency;
 Console.WriteLine("before wait all");
 TaskDemo.MyWaitAll(Enumerable.Range(0, 10).Select(i => Task.Run(() => { Thread.Sleep(i + 1000); Console.WriteLine(i);})).ToArray());
 Console.WriteLine("after wait all");
+
+
+Console.WriteLine("before when all");
+var t = TaskDemo.MyWhenAll(Enumerable.Range(0, 10).Select(i => Task.Run(() => { Thread.Sleep(i + 1000); Console.WriteLine(i);})).ToArray());
+Console.WriteLine("after when all");
+t.Wait();
+Console.WriteLine("after wait when all task");
